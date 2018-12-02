3174728e1bdd9e82414e07fb89d756872e87084f
dzyls <1217167420@qq.com>
2018-12-02 10:08:28 +0800

    test

--- src/com/solution/leetcode2/RotatedDigits.java
+++ src/com/solution/leetcode2/RotatedDigits.java
@@ -18,7 +18,7 @@ public class RotatedDigits {
         map.put(0,0);
         map.put(1,1);
         map.put(5,2);
-        map.put(2,5);
+        map.put(2,5); 
         map.put(6,9);
         map.put(9,6);
         map.put(8,8);
