05ba59cd2a7cc61e757c0f4c21aef98d838c77dc
xdhdedie <627948550@qq.com>
2018-12-02 10:25:56 +0800

    this is a message

--- src/com/solution/leetcode/AddDigits.java
+++ src/com/solution/leetcode/AddDigits.java
@@ -13,7 +13,7 @@ public class AddDigits {
         char[] chars = s.toCharArray();
         int n = 0;
         for (char c : chars) {
-            n += (c - '0'); 
+            n += (c - '0');  
         }
         if (n /10 == 0){
             return n;
